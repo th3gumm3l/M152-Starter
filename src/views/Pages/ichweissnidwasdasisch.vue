b7f876365b26ed88d1f5c37bbaa54160a4054fdc
Tim Bernhard <tim.bernhard@me.com>
2023-05-10 12:55:52 +0200

    reformatted code

--- src/views/Pages/ichweissnidwasdasisch.vue
+++ src/views/Pages/ichweissnidwasdasisch.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="people">
         <li v-for="index in 10" :key="index" class="person">
-            <img class="person__image" src="https://source.unsplash.com/300x300/?portrait" />
+            <img class="person__image" src="https://source.unsplash.com/300x300/?portrait"/>
             <div class="person__inner">
                 <div class="person__title">
                     Andi Moser
@@ -15,52 +15,61 @@
     </ul>
 </template>
 <style lang="scss">
-.people {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 10px;
-  list-style-type: none;
-  .person {
-    &__image {
-      border-radius: 50%;
-      width: 100%;
+    .people {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 10px;
+        list-style-type: none;
+
+    .person {
+        margin-bottom: 40px;
+        &__image {
+          border-radius: 50%;
+          width: 100%;
+        }
+
+        &__inner {
+          text-align: center;
+        }
+
+        &__title {
+          font-size: 2rem;
+          font-weight: bold;
+        }
+
+        &__button {
+          background-color: teal;
+          border-radius: 5px;
+          color: white;
+          padding: 10px;
+          border: none;
+          margin: 30px 0 0;
+        }
     }
-    &__inner {
-      text-align: center;
-    }
-    &__title {
-      font-size: 2rem;
-      font-weight: bold;
-    }
-    &__button {
-      background-color: teal;
-      border-radius: 5px;
-      color: white;
-      padding: 10px;
-      border: none;
-      margin: 30px 0 0;
-    }
-    margin-bottom: 40px;
-  }
 }
 
 @media only screen and (max-width: 768px) {
     .people {
         grid-template-columns: 1fr 1fr;
-        .person{
-            display: flex;
+
+        .person {
+          display: flex;
         }
-        .person__image{
-            width: 50%;
+
+        &__image {
+          width: 50%;
         }
-        .person__title{
-            font-size: 1.5rem;
+
+        &__title {
+          font-size: 1.5rem;
         }
-        .person__button {
-            margin: 5px 0 0;
+
+        &__button {
+          margin: 5px 0 0;
         }
-        .person__inner {
-            padding-top: 25px;
+
+        &__inner {
+          padding-top: 25px;
         }
     }
 }
@@ -69,9 +78,10 @@
     .people {
         grid-template-columns: 1fr;
         padding: 0;
+
         .person {
-            flex-direction: column;
-            align-items: center;
+          flex-direction: column;
+          align-items: center;
         }
     }
 }
